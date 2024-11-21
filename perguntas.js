1389130ff8b60a57b3b587c2fc9b15482e00a11b
cclarafs <00001096346278sp@al.educacao.sp.gov.br>
2024-11-21 11:58:44 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Quimica',
+    'Qual é a fórmula da água?',
+    'H₂O'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o planeta mais próximo do Sol?',
+    'Mercúrio'
+)
+
+criaCartao(
+    'Portugues',
+    'Quem escreveu Dom Casmurro?',
+    'Machado de Assis'
+)
+
+criaCartao(
+    'Matematica',
+    'Qual é o ângulo interno de um triângulo equilátero?',
+    '60°'
+)
